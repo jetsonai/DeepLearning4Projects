df425cf8e3c04dddbe21d1c94e96e99a7df9cf9c
JinFree <ytrqwe12@gmail.com>
2022-01-24 03:23:18 +0900

    inference on jetson

--- Chap7/build_darknet_on_jetson_nano.sh
+++ Chap7/build_darknet_on_jetson_nano.sh
@@ -4,3 +4,7 @@ cp Makefile_Nano darknet/Makefile
 cd darknet && make -j $(nproc)
 wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.weights
 ./darknet detect cfg/yolov4-tiny.cfg yolov4-tiny.weights data/dog.jpg
+mv vocnames.txt darknet/
+mv vocdata.txt darknet/
+mv yolov4-tiny_voc.cfg darknet/
+mv yolov4-tiny_voc_last.weights darknet/
