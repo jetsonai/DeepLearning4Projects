87748c60e1f02f4d6e25bb141a0bcb65c1ce6620
JinFree <ytrqwe12@gmail.com>
2023-05-13 19:32:55 +0900

    prepare env chap 10

--- Chap10/prepare_env_in_jetson.sh
+++ Chap10/prepare_env_in_jetson.sh
@@ -3,5 +3,5 @@ cd ~
 git clone -b v7.0 https://github.com/jetsonai/yolov5
 git clone -b yolov5-v7.0 https://github.com/jetsonai/tensorrtx
 python3 -m pip install --upgrade pip
-python3 -m pip install tqdm thop seaborn
+python3 -m pip install tqdm thop seaborn ipython psutil
 cp ~/tensorrtx/yolov5/gen_wts.py ~/yolov5/.
