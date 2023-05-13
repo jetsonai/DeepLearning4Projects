42e03f4621dd76ff8ba0b2f8d7a93af251a9bbb5
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2023-05-13 14:02:39 +0900

    Create installPyTorch1_8.sh

---
+++ StartJetsonNano/installPyTorch1_8.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd 
+wget https://nvidia.box.com/shared/static/p57jwntv436lfrd78inwl7iml6p13fzh.whl -O torch-1.8.0-cp36-cp36m-linux_aarch64.whl
+sudo apt-get install python3-pip libopenblas-base libopenmpi-dev
+
+python3 -m pip install Cython 
+python3 -m pip install wheel
+python3 -m pip install numpy
+python3 -m pip install torch-1.8.0-cp36-cp36m-linux_aarch64.whl
+rm torch-1.8.0-cp36-cp36m-linux_aarch64.whl
+
+
+sudo apt-get install libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev -y
+git clone --branch v0.9.0 https://github.com/pytorch/vision torchvision
+cd torchvision
+export BUILD_VERSION=0.9.0
+sudo python3 setup.py install --user
+cd ../  # attempting to load torchvision from build dir will result in import error
+sudo rm -rf torchvision
