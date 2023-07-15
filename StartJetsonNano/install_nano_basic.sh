3b595d8e1de76ba6b9d2358346da4a9748f8ee3d
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2023-07-15 21:02:42 +0900

    Update install_nano_basic.sh

--- StartJetsonNano/install_nano_basic.sh
+++ StartJetsonNano/install_nano_basic.sh
@@ -3,7 +3,6 @@ sudo apt-get update
 
 sudo apt-get install libpython3-dev -y
 
-
 sudo apt install python-dev python3-dev python-pip python3-pip python-numpy python3-numpy \
   python3-matplotlib python3-venv -y
 sudo -H pip install -U jetson-stats
@@ -13,4 +12,7 @@ sudo apt install libcanberra-gtk-module libcanberra-gtk3-module -y
 pip3 install tqdm
 
 pip3 install packaging
+
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/cuda/lib64" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/local/cuda/bin" >> ~/.bashrc
 
