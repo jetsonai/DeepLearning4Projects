3fa2842d9969bc105162e18b4fec4ad8f5e9e030
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2023-07-15 16:37:57 +0900

    Add files via upload

---
+++ Chap8_4/build_darknet_on_jetson_nano.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git clone https://github.com/jetsonai/darknet.git
+mv vocnames.txt darknet/
+mv vocdata.txt darknet/
+mv yolov4-tiny_voc.cfg darknet/
+mv yolov4-tiny_voc_last.weights darknet/
+mv darknet_video.py darknet/
+mv blackbox_video.avi darknet/
+cp Makefile_Nano darknet/Makefile
+cd darknet && make -j $(nproc)
+wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.weights
+./darknet detect cfg/yolov4-tiny.cfg yolov4-tiny.weights data/dog.jpg
