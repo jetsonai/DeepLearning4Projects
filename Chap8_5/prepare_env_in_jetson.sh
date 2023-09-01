59ce777d0947da320ae2b576d079f3b548e972b2
JinFree <ytrqwe12@gmail.com>
2023-09-02 00:35:01 +0900

    yolov5

--- Chap8_5/prepare_env_in_jetson.sh
+++ Chap8_5/prepare_env_in_jetson.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+sudo apt install libblas-dev liblapack-dev libatlas-base-deb gfortran
+python3 -m pip install cython==0.29.15
+python3 -m pip install numpy==1.18.5
+python3 -m pip install scipy==1.4.1
+python3 -m pip install seaborn==0.11.0
+python3 -m pip install pandas==1.1.4
+cp weights/yolov5s_voc.pt ~/yolov5s_voc.pt
 cd ~
 git clone -b v7.0 https://github.com/jetsonai/yolov5
 git clone -b yolov5-v7.0 https://github.com/jetsonai/tensorrtx
 python3 -m pip install --upgrade pip
-python3 -m pip install tqdm thop seaborn ipython psutil 
+python3 -m pip install tqdm thop ipython psutil 
 cp ~/tensorrtx/yolov5/gen_wts.py ~/yolov5/.
+cp ~/yolov5s_voc.pt ~/yolov5/.
 wget https://files.pythonhosted.org/packages/5e/3f/5658c38579b41866ba21ee1b5020b8225cec86fe717e4b1c5c972de0a33c/pycuda-2019.1.2.tar.gz
 tar xvf pycuda-2019.1.2.tar.gz
 cd pycuda-2019.1.2
