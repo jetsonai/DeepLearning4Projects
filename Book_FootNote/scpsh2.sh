e335c5602bb1fb5f9ce11c4f721cb2cdc83d0cb7
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2024-06-26 11:40:31 +0900

    Add files via upload

---
+++ Book_FootNote/scpsh2.sh
@@ -0,0 +1,15 @@
+
+scp -r sensor_test_pack nvidia@192.168.100.60:/home/nvidia/sensor_ws/src/
+
+scp -r sensor_test_pack nvidia@192.168.100.93:/home/nvidia/sensor_ws/src
+
+scp -r sensor_test_pack nvidia@192.168.100.23:/home/nvidia/sensor_ws/src
+
+scp -r sensor_test_pack nvidia@192.168.100.92:/home/nvidia/sensor_ws/src
+
+scp -r sensor_test_pack nvidia@192.168.100.91:/home/nvidia/sensor_ws/src
+
+scp -r sensor_test_pack nvidia@192.168.100.90:/home/nvidia/sensor_ws/src
+
+scp -r sensor_test_pack nvidia@192.168.100.88:/home/nvidia/sensor_ws/src
+
