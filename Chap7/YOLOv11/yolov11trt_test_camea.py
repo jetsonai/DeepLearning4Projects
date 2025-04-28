f301a7560bc9f601e97576b41599b037b1e6abc8
R SSAEM <96120477+jetsonai@users.noreply.github.com>
2025-04-28 21:50:13 +0900

    Add files via upload

---
+++ Chap7/YOLOv11/yolov11trt_test_camea.py
@@ -0,0 +1,46 @@
+import ultralytics
+import cv2
+from PIL import Image
+import numpy as np
+
+#gst_str = ("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)320, height=(int)240, format=(string)NV12, framerate=(fraction)60/1 ! nvvidconv flip-method=0 ! video/x-raw, width=(int)320, height=(int)240, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+gst_usb_str = ("v4l2src device=/dev/video0 ! video/x-raw, width=640, height=480, format=(string)YUY2,framerate=30/1 ! videoconvert ! video/x-raw,width=640,height=480,format=BGR ! appsink")
+
+ultralytics.checks()
+
+from ultralytics import YOLO
+
+trt_model = YOLO("yolo11n.engine")
+
+#results = model(source = 0, show=True, conf=0.3, save=True)
+
+#video_source = "cctv.mp4"
+#video_source = gst_str
+video_source = gst_usb_str
+
+cap = cv2.VideoCapture(video_source)
+if cap.isOpened():
+    print("Video Opened")
+else:
+    print("Video Not Opened")
+    print("Program Abort")
+    exit()
+#cv2.namedWindow("YOLOv8", cv2.WINDOW_GUI_EXPANDED)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+        #cv2.imshow("RAW", frame)
+        
+        results = trt_model.predict(frame, imgsz=480)
+        annotated_frame = results[0].plot()
+        cv2.imshow("YOLOv11", annotated_frame)
+        
+    else:
+        break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
